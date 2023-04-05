3aeb4310064cfcf7fd192776b2916e4c9e69e299
Abhra303 <chakrabortyabhradeep79@gmail.com>
2023-04-06 03:59:59 +0530

    feat: enable parsing and running the given command

--- cmd/handler.go
+++ cmd/handler.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/Abhra303/quickmap/pkg/parse"
+	"github.com/efficientgo/core/errors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,9 +13,13 @@ type request struct {
 }
 
 type response struct {
-	Code  int    `json:"code"`
-	Value string `json:"value,omitempty"`
-	Err   string `json:"error,omitempty"`
+	Code  int         `json:"code"`
+	Value interface{} `json:"value,omitempty"`
+	Err   string      `json:"error,omitempty"`
+}
+
+func badReqResponse(c *gin.Context, err error) {
+	c.JSON(http.StatusBadRequest, &response{Code: http.StatusBadRequest, Err: err.Error()})
 }
 
 func v1ExecCommandHandler(c *gin.Context) {
@@ -21,6 +27,46 @@ func v1ExecCommandHandler(c *gin.Context) {
 	var err error
 
 	if err = c.BindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, &response{Code: http.StatusBadRequest, Err: err.Error()})
+		badReqResponse(c, err)
+	}
+
+	expr, err := parse.ParseCommand(req.Command)
+	if err != nil {
+		badReqResponse(c, err)
+	}
+	switch e := expr.(type) {
+	case *parse.SetExpr:
+		err = Store.Set(e.Key, e.Value, e.Expiry, e.Condition)
+		if err != nil {
+			badReqResponse(c, err)
+		}
+		c.JSON(http.StatusCreated, &response{Code: http.StatusCreated})
+	case *parse.GetExpr:
+		value, ok := Store.Get(e.Key)
+		if !ok {
+			badReqResponse(c, err)
+		}
+		c.JSON(http.StatusOK, &response{Code: http.StatusOK, Value: value})
+	case *parse.QPushExpr:
+		err = Store.QPush(e.Key, e.Values)
+		if err != nil {
+			badReqResponse(c, err)
+		}
+		c.JSON(http.StatusCreated, &response{Code: http.StatusCreated})
+	case *parse.QPopExpr:
+		value, err := Store.QPop(e.Key)
+		if err != nil {
+			badReqResponse(c, err)
+		}
+		c.JSON(http.StatusOK, &response{Code: http.StatusOK, Value: value})
+	case *parse.BQPopExpr:
+		value, err := Store.BQPop(e.Key, e.Timeout)
+		if err != nil {
+			badReqResponse(c, err)
+		}
+		c.JSON(http.StatusOK, &response{Code: http.StatusOK, Value: value})
+	default:
+		err = errors.New("unknown expression type")
+		badReqResponse(c, err)
 	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/Abhra303/quickmap/pkg/datastore"
 	"github.com/gin-gonic/gin"
 )
 
+var Store datastore.DataStore
+
 func main() {
+	Store = datastore.NewDataStore()
 	// start server
 	router := gin.Default()
 
